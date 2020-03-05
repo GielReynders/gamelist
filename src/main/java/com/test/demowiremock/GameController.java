295e4437c6a497fb937f410b490018bde88dcb8a
Giel Reynders <Reynders_giel@hotmail.com>
2020-03-05 22:08:05 +0100

    Use wireMock to mock a webClient in my controller during a test

--- src/main/java/com/test/demowiremock/GameController.java
+++ src/main/java/com/test/demowiremock/GameController.java
@@ -1,33 +1,26 @@
 package com.test.demowiremock;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/games")
 public class GameController {
 
-    private final WebClient todoWebClient;
-
-    public GameController(WebClient todoWebClient) {
-        this.todoWebClient = todoWebClient;
-    }
-
-
     @GetMapping()
     public ResponseEntity<String> getAllGames() {
-//        this.todoWebClient
-//                .get()
-//                .uri(url)
-//                .exchange()
-//                .expectStatus()
-//                .is2xxSuccessful()
-//                .expectBody()
-//                .jsonPath("$[0].title")
-//                .isEqualTo("Diablo 1")
-//                .jsonPath("$.length()")
-//                .isEqualTo(2);
-        return ResponseEntity.ok("{\"game\":\"Diablo_2\"}");
+        final String url = "http://localhost:8081/game-list";
+        final WebClient webClient = WebClient.create("http://localhost:8081");
+
+        final WebClient.RequestBodySpec uri = webClient.method(HttpMethod.GET).uri("/game-list");
+
+        final Flux<String> stringFlux = uri.exchange().block().bodyToFlux(String.class);
+
+        return ResponseEntity.ok(stringFlux.blockFirst());
     }
 }
--- src/test/java/com/test/demowiremock/GameControllerTest.java
+++ src/test/java/com/test/demowiremock/GameControllerTest.java
@@ -2,25 +2,16 @@ package com.test.demowiremock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.cloud.contract.wiremock.WireMockRestServiceServer;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.client.RestTemplate;
-import wiremock.com.google.common.util.concurrent.Service;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = {WireMockInitializer.class})
@@ -36,7 +27,7 @@ class GameControllerTest {
     private Integer port;
 
     @AfterEach
-    public void afterEach() {
+    void afterEach() {
         this.wireMockServer.resetAll();
     }
 
@@ -51,7 +42,7 @@ class GameControllerTest {
                                         "{\"gameId\": 1,\"id\": 2,\"title\": \"Diablo 2\", \"completed\": true}]"))
         );
 
-        String url = "http://localhost:8081/games";
+        final String url = "http://localhost:8081/games";
 
         this.webTestClient
                 .get()
@@ -66,4 +57,28 @@ class GameControllerTest {
                 .isEqualTo(2);
     }
 
+    @Test
+    void getAllGamesCallLocalController_ReturnsListOfGames() {
+        this.wireMockServer.stubFor(
+                WireMock.get("/game-list")
+                        .willReturn(aResponse()
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody("[{\"gameId\": 1,\"id\": 1,\"title\": \"Diablo 1\", \"completed\": false}," +
+                                        "{\"gameId\": 1,\"id\": 2,\"title\": \"Diablo 2\", \"completed\": true}]"))
+        );
+
+        final String url = "http://localhost:" + port + "/api/games";
+
+        this.webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$[0].title")
+                .isEqualTo("Diablo 1")
+                .jsonPath("$.length()")
+                .isEqualTo(2);
+    }
 }
