6b09d5b1a4768d9ed51eb96ff2756a8ced686397
Giel Reynders <Reynders_giel@hotmail.com>
2020-03-05 22:08:05 +0100

    Use wireMock to mock a webClient in my controller during a test

--- src/main/java/com/test/demowiremock/DemoWireMockApplication.java
+++ src/main/java/com/test/demowiremock/DemoWireMockApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoWiremockApplication {
+public class DemoWireMockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoWiremockApplication.class, args);
+		SpringApplication.run(DemoWireMockApplication.class, args);
 	}
 
 }
